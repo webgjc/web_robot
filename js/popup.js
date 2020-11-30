3d2ca4d9ee2c212cdc13a4c40d59ff9103832f1e
甘家城 <935669873@qq.com>
2020-11-30 18:10:46 +0800

    bugfix:修改title不存在情况

--- js/popup.js
+++ js/popup.js
@@ -1209,7 +1209,12 @@ $(document).ready(function () {
                                 check: true
                             }
                         ];
-                        let name = tab.title.replaceAll(" ", "") + "_" + getCurrentDateTime();
+                        let name;
+                        if (tab.title) {
+                            name = tab.title.replaceAll(" ", "") + "_" + getCurrentDateTime();
+                        } else {
+                            name = "看板" + "_" + getCurrentDateTime();
+                        }
                         get_my_robot(my_robot => {
                             my_robot[name] = {
                                 case_name: name,
